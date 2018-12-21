b3cd0f5205d722dccca841081288b5a2a73fd189
gabrezues <43757111+gabrezues@users.noreply.github.com>
2018-12-21 10:08:56 -0500

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+print('-'*60)
+print('Iz Brick Bot:')
+print()
+
+temp = input('Temperature in Farenheit: ')
+temp = int(temp)
+
+if temp >= 32:
+	print('Its not brick, enjoy your day bitch')
+
+else:
+	print('Its brick! bring ya jacket')
+
+print('-'*60)
---
+++ problem2.py
@@ -0,0 +1,15 @@
+print('-'*60)
+print('Narrative Bot')
+print()
+
+name = input('Student Name: ')
+grade = input('Grade: ')
+grade = int(grade)
+print()
+print('Narrative')
+
+if grade >= 65:
+	print('You are doing sufficient work to pass this class and I appreciate your hard work.')
+
+else:
+	print('You have not done enough work in this class to pass. Now you must take an MBA assignment next semester.')
